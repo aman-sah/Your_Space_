9dc698fd4aa9ccf7e2b90bd337e5511a6ce941cd
aman-sah <amankumarmdp88@gmail.com>
2026-05-26 11:29:37 +0530

    updated db.js file

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,24 +9,3 @@ export const connectDB = async () => {
         process.exit(1);
     }
 };
-
-
-// import mongoose from "mongoose";
-
-// export const connectDB = async () => {
-//     try {
-//         await mongoose.connect("mongodb+srv://amankumarmdp88_db_user:wJFFR7tMaPweYJZt@cluster0.ec6rwiq.mongodb.net/YourSpace")
-//         .then(()=>{
-//             console.log("DB Connected");
-//         })
-        
-//     } catch (error) {
-//         console.error("DB Connection Failed:", error.message);
-//         process.exit(1);
-//     }
-// };
-
-
-
-//amankumarmdp88_db_user
-//wJFFR7tMaPweYJZt
